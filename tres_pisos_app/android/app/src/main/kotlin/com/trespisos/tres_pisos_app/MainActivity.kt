136bf95d8db40d98f42bdb212fcd01c8d34947bf
Raul Carrasco <raulcarrasco448@gmail.com>
2026-09-22 13:07:23 -0600

    chore: eliminar la app Flutter para rehacerla desde cero

--- tres_pisos_app/android/app/src/main/kotlin/com/trespisos/tres_pisos_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.trespisos.tres_pisos_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- tres_pisos_app/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-// test/widget_test.dart
-import 'package:flutter_test/flutter_test.dart';
-import 'package:tres_pisos_app/main.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const TresPisosApp());
-  });
-}
